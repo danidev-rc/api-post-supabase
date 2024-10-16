d658e3d8352fad78ae552cbb35a914415b325381
Dani <dantesamuelrodriguez@gmail.com>
2024-10-15 23:51:07 -0500

    aixos returned

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,16 +3,7 @@ import { API_URL } from "../config";
 
 const instance = axios.create({
   baseURL: API_URL,
-  withCredentials: true,  // Esto permite el envío de cookies de autenticación
-});
-
-// Añade un interceptor para agregar el token en cada solicitud si es necesario
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // O donde almacenes tu token
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+  withCredentials: true,
+})
 
 export default instance;

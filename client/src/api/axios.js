a13f739aaffd70878f71a2fd1808e34b8e2462ca
Dani <dantesamuelrodriguez@gmail.com>
2024-10-15 23:35:18 -0500

    axios changed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 import { API_URL } from "../config";
+import Cookies from 'js-cookie';
+
+const token = Cookies.get('authToken');
 
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
+  headers: {
+    "Authorization": `Bearer ${token}`
+  }
 })
 
 export default instance;
